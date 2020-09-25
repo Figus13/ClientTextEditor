b92f71dc26cb3a5ad23c99720b3e987655f280f7
MicheleTatti <micheletatti94@gmail.com>
2020-09-25 16:39:12 +0200

    Aggiunto pop up dialog

--- PDSClient/mainwindow.cpp
+++ PDSClient/mainwindow.cpp
@@ -2,9 +2,9 @@
 #include "ui_mainwindow.h"
 #include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent, Client *client)
+MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow), client(client)
+    , ui(new Ui::MainWindow)
 {
 
     ui->setupUi(this);
